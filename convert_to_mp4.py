542569afa93c105d6f667f8a4de275a9e20d145c
Kenneth Preston <kenneth.j.preston@gmail.com>
2023-01-23 16:27:54 -0500

    Update convert_to_mp4.py

--- convert_to_mp4.py
+++ convert_to_mp4.py
@@ -12,8 +12,5 @@ def convert_movie(file, fileout='myvideo.mp4'):
     src_dir=file
     dst_dir='/tmp/{}.'.format(unique_filename)+file.split('.')[-1]
     filename = shutil.copy(src_dir,dst_dir)
-    #filename2 = filename.replace(file.split('.')[-1],'mp4')
-    #     with open(filename2, 'w') as fp:
-    #         pass
     query = '''ffmpeg -i {} -vcodec libx264 -vf "fps=25" -vsync cfr -b:v 2300k -acodec aac -b:a 128k {} -y'''.format(filename, fileout.replace(' ','_'))
     subprocess.run(query, shell=True)
