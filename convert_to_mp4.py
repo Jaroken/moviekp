50d449f8af43a40b262dee970a7950c4b35ef11a
Kenneth Preston <kenneth.j.preston@gmail.com>
2023-01-23 16:27:40 -0500

    Update convert_to_mp4.py

--- convert_to_mp4.py
+++ convert_to_mp4.py
@@ -3,7 +3,6 @@ import subprocess
 import uuid
 
 # need ffmpeg installed (apt-get install ffmpeg)
-# designed to work on linux
 def convert_movie(file, fileout='myvideo.mp4'):
     """Convert file to mp4. need ffmpeg installed (apt-get install ffmpeg)
     parameters:
@@ -13,12 +12,8 @@ def convert_movie(file, fileout='myvideo.mp4'):
     src_dir=file
     dst_dir='/tmp/{}.'.format(unique_filename)+file.split('.')[-1]
     filename = shutil.copy(src_dir,dst_dir)
-    filename2 = filename.replace(file.split('.')[-1],'mp4')
-    with open(filename2, 'w') as fp:
-        pass
-    query = '''ffmpeg -i {} -vcodec libx264 -vf "fps=25" -vsync cfr -b:v 2300k -acodec aac -b:a 128k {} -y'''.format(filename, filename2)
+    #filename2 = filename.replace(file.split('.')[-1],'mp4')
+    #     with open(filename2, 'w') as fp:
+    #         pass
+    query = '''ffmpeg -i {} -vcodec libx264 -vf "fps=25" -vsync cfr -b:v 2300k -acodec aac -b:a 128k {} -y'''.format(filename, fileout.replace(' ','_'))
     subprocess.run(query, shell=True)
-    
-    src_dir= filename2
-    dst_dir= fileout
-    filename = shutil.copy(src_dir,dst_dir) 
