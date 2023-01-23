77d9352543581939c3dbcd84729c341d77afe17a
Kenneth Preston <kenneth.j.preston@gmail.com>
2023-01-23 15:55:51 -0500

    Create convert_to_mp4.py

---
+++ convert_to_mp4.py
@@ -0,0 +1,24 @@
+import shutil
+import subprocess
+import uuid
+
+# need ffmpeg installed (apt-get install ffmpeg)
+# designed to work on linux
+def convert_movie(file, fileout='myvideo.mp4'):
+    """Convert file to mp4. need ffmpeg installed (apt-get install ffmpeg)
+    parameters:
+    file: path to movie file
+    fileout: filename of outfile (default is myvideo.mp4)"""
+    unique_filename = str(uuid.uuid4())
+    src_dir=file
+    dst_dir='/tmp/{}.'.format(unique_filename)+file.split('.')[-1]
+    filename = shutil.copy(src_dir,dst_dir)
+    filename2 = filename.replace(file.split('.')[-1],'mp4')
+    with open(filename2, 'w') as fp:
+        pass
+    query = '''ffmpeg -i {} -vcodec libx264 -vf "fps=25" -vsync cfr -b:v 2300k -acodec aac -b:a 128k {} -y'''.format(filename, filename2)
+    subprocess.run(query, shell=True)
+    
+    src_dir= filename2
+    dst_dir= fileout
+    filename = shutil.copy(src_dir,dst_dir) 
